eb726779ee10a1972aca109e1aaa67c54d854ec5
phasel <peter.hasel@gmail.com>
2020-03-07 21:16:39 +0100

    add menu cartes

--- parsers/index.js
+++ parsers/index.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const fs = require('fs')
+
 const cafe_museum = require("./cafe-museum")
 const eiszeit = require("./eiszeit")
 const hausmann = require("./hausmann")
@@ -6,37 +9,45 @@ const schuler = require("./schuler")
 const sonne = require("./sonne")
 
 function parse(name) {
-    return new Promise((resolve, reject) => {
-      // @todo more elegant way?
-      switch (name) {
-        case "schuler":
-        case "schuler-kantine":
-          schuler.parse().then(resolve);
-          break;
-        case "hausmann":
-        case "baeckerei-hausmann":
-          hausmann.parse().then(resolve);
-          break;
-        case "hotel-sonne":
-        case "sonne":
-          sonne.parse().then(resolve);
-          break;
-        case "cafe-museum":
-        case "museum":
-          cafe_museum.parse().then(resolve);
-          break;
-        //case "kbzo-kantine":
-        //case "kbzo":
-        //kbzo.parse().then(resolve)
-        //break;
-        case "eiszeit":
-          eiszeit.parse().then(resolve);
-          break;
-        default:
-          reject({ error: "unknown parse function" + name + "!" });
+  return new Promise((resolve, reject) => {
+    // @todo more elegant way?
+    switch (name) {
+      case "schuler":
+      case "schuler-kantine":
+        schuler.parse().then(resolve);
+        return;
+      case "hausmann":
+      case "baeckerei-hausmann":
+        hausmann.parse().then(resolve);
+        return;
+      case "hotel-sonne":
+      case "sonne":
+        sonne.parse().then(resolve);
+        return;
+      case "cafe-museum":
+      case "museum":
+        cafe_museum.parse().then(resolve);
+        return;
+      //case "kbzo-kantine":
+      //case "kbzo":
+      //kbzo.parse().then(resolve)
+      //return;
+      case "eiszeit":
+        eiszeit.parse().then(resolve);
+        return;
+      default:
+      //reject({ error: "unknown parse function '" + name + "'!" });
+    }
+    // no parse function defined - try to load dishes json
+    fs.readFile(path.join(__dirname, '../dishes/' + name + '.json'), 'utf8', (err, jsonString) => {
+      if (err) {
+        reject({ error: "no parse function or json for '" + name + "'!" });
+        return
       }
-    });
-  }
+      resolve({ ...JSON.parse(jsonString) })
+    })
+  });
+}
 
-  //
-  exports.parse = parse;
+//
+exports.parse = parse;
